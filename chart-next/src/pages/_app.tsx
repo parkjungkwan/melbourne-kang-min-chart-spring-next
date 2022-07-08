3cac9485e905395a9cf9bc7fe4569c63e96b8564
BeautyKim <kima2012114@gmail.com>
2022-07-08 15:52:45 +0900

    홈 버튼

--- chart-next/src/pages/_app.tsx
+++ chart-next/src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { wrapper } from 'modules/store'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <Link href="/"><a>Home</a></Link>
+    <Component {...pageProps} />
+  </>
+  )
 }
 
 export default wrapper.withRedux(MyApp) 
